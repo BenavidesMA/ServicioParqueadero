187bdda322cd7f6ab05b3b83048bdc10c934e230
Miguel <miguelbv2302@gmail.com>
2025-11-17 19:22:59 -0500

    Se creó la estructura base del proyecto

---
+++ src/main/java/Vista/ServicioParqueadero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Vista;
+
+/**
+ *
+ * @author Miguel
+ */
+public class ServicioParqueadero {
+
+    public static void main(String[] args) {
+        System.out.println("Hola como estan todos");
+    }
+}
