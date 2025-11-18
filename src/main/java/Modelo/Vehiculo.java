e515979d0c203456f5de426336b6ae537092535f
lizqui29 <lizeth_isa.quinones@uao.edu.co>
2025-11-17 22:17:51 -0500

    Se han creado atributos en la clase Vehiculo del paquete modelo, junto a su constructor, getters y setters.

--- src/main/java/Modelo/Vehiculo.java
+++ src/main/java/Modelo/Vehiculo.java
@@ -4,10 +4,74 @@
  */
 package Modelo;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Usuario
  */
 public abstract class Vehiculo {
+    protected String placa;
+    protected String modelo;
+    protected Propietario propietario;
+    protected LocalDateTime horaEntrada;
+    protected LocalDateTime horaSalida;
+
+    public Vehiculo(String placa, String modelo, Propietario propietario, LocalDateTime horaEntrada, LocalDateTime horaSalida) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.propietario = propietario;
+        this.horaEntrada = horaEntrada;
+        this.horaSalida = horaSalida;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Propietario getPropietario() {
+        return propietario;
+    }
+
+    public LocalDateTime getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public LocalDateTime getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+    }
+
+    public void setHoraEntrada(LocalDateTime horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public void setHoraSalida(LocalDateTime horaSalida) {
+        this.horaSalida = horaSalida;
+    }
     
+    public void registrarEntrada() {
+        this.horaEntrada = LocalDateTime.now();
+    }
+
+    public void registrarSalida() {
+        this.horaSalida = LocalDateTime.now();
+    }
+
+    public abstract double calcularTarifa();
 }
